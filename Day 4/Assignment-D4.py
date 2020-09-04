cae6914de860aae4d016559b76460e2256a74a83
ankiyfaltu <70653633+ankiyfaltu@users.noreply.github.com>
2020-09-04 23:18:43 +0530

    Create Assignment-D4.py
    Assignment of day 4 completed

---
+++ Day 4/Assignment-D4.py
@@ -0,0 +1,14 @@
+# Program for finding first Armstrong number in the given range
+start = 1042000
+end = 702648265
+for i in range(start, end+1):
+    sum = 0
+    temp_numb = i
+    power = len(str(i))
+    while temp_numb > 0:
+        number = temp_numb % 10
+        sum += number ** power
+        temp_numb //= 10
+    if i == sum:
+        print(i, 'is the first armstrong number')
+        break
